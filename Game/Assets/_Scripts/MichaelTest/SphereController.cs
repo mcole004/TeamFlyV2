abdf1ceda44dbab9cd40eda2df20fbf9cd9c87a5
mcole004 <mcole004@ucr.edu>
2015-01-31 14:26:56 -0800

    Reorganized folder
    I moved some stuff around and added a few things. Im going to use my own
    scene to create and test stuff and then add it to the main scene later.

---
+++ Game/Assets/_Scripts/MichaelTest/SphereController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereController : MonoBehaviour {
+	
+	public float speed = 500;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+		
+	}
+}
---
+++ Game/Assets/_Scripts/MichaelTest/colorchange.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colorchange : MonoBehaviour {
+
+	MeshRenderer cube_shader;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		cube_shader = GetComponent <MeshRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cube_shader.material.color = Color.green;
+	}
+}
