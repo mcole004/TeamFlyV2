ec5103085eb648c21b3619fcb88d3825011e1cfc
flyon001 <flyon001@student.ucr.edu>
2015-02-19 18:00:55 -0800

    Doors and pulsing lights
    doors in map with pulsing lights over puzzle pieces

---
+++ Game/Assets/_Scripts/Doors/door8.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class door8 : MonoBehaviour {
+
+	private Transform doorHingeRoom1;
+	
+	public float doorSpeed = 5f;
+	
+	public Quaternion open = Quaternion.identity;
+	
+	
+	void Awake()
+	{
+		//doorHingeRoom1 = GameObject.Find ("Hinge0").transform;
+		doorHingeRoom1 = GameObject.FindGameObjectWithTag ("Hinge8").transform;
+		//want the door to be in closed position
+		open.eulerAngles = new Vector3 (0,0,0);
+		
+		//gameObject.renderer.material.color = Color.gray;
+	}
+	
+	void Update () 
+	{
+		//the update always triggering the door to stay closed 
+		doorHingeRoom1.rotation = Quaternion.Lerp(doorHingeRoom1.rotation,open,Time.deltaTime * doorSpeed);
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player") 
+		{
+			//The posoition the door is fully opened at
+			open.eulerAngles= new Vector3(0,90,0);
+			
+		}
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			//The position where the door is closed at
+			open.eulerAngles = new Vector3(0,0,0);
+		}
+		
+		
+	}
+}
