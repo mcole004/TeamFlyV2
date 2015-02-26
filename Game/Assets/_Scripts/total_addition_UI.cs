08107e482e33072461c149acb4f39ef4cc921eea
mcole004 <mcole004@ucr.edu>
2015-02-26 14:17:33 -0800

    Added UI and Misc
    UI and floor panels for doors

---
+++ Game/Assets/_Scripts/total_addition_UI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class total_addition_UI : MonoBehaviour {
+
+	public static int total;        
+	
+	Text text;                      
+	
+	void Awake ()
+	{
+		// Set up the reference.
+		text = GetComponent <Text> ();
+		
+		// Reset the score.
+		total = 0;
+	}
+	
+	
+	void Update ()
+	{
+		// Set the displayed text to be the word "Score" followed by the score value.
+		text.text = " " + total;
+	}
+}
