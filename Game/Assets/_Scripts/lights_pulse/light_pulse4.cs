f8c0a145e54b149fbbfbf78537e76a9d797d907c
mcole004 <mcole004@ucr.edu>
2015-02-25 18:33:02 -0800

    Merge remote-tracking branch 'origin/Demo2' into Michael
    Conflicts:
    	Game/Assembly-CSharp-vs.csproj
    	Game/Assembly-CSharp.csproj
    	Game/Assets/Scences/Map.unity.meta
    	Game/Assets/_Scenes/Demo2.unity
    	Game/Assets/_Scenes/Demo2.unity.meta
    	Game/Assets/_Scenes/Map.unity.meta
    	Game/Assets/_Scenes/MichaelTestScene1.unity
    	Game/Assets/_Scenes/MichaelTestScene1.unity.meta
    	Game/Assets/_Scripts.meta
    	Game/Assets/_Scripts/MichaelTest.meta
    	Game/Assets/_Scripts/MichaelTest/CameraScript.cs
    	Game/Assets/_Scripts/MichaelTest/CameraScript.cs.meta
    	Game/Assets/_Scripts/MichaelTest/SphereController.cs
    	Game/Assets/_Scripts/MichaelTest/SphereController.cs.meta
    	Game/Assets/_Scripts/MichaelTest/colorchange.cs
    	Game/Assets/_Scripts/MichaelTest/colorchange.cs.meta
    	Game/Assets/_Scripts/MichaelTest/cube_contBL.cs
    	Game/Assets/_Scripts/MichaelTest/cube_contBL.cs.meta
    	Game/Assets/_Scripts/MichaelTest/cube_contBM.cs
    	Game/Assets/_Scripts/MichaelTest/cube_contBM.cs.meta
    	Game/Assets/_Scripts/MichaelTest/cube_contBR.cs
    	Game/Assets/_Scripts/MichaelTest/cube_contBR.cs.meta
    	Game/Assets/_Scripts/MichaelTest/cube_contML.cs
    	Game/Assets/_Scripts/MichaelTest/cube_contML.cs.meta
    	Game/Assets/_Scripts/MichaelTest/cube_contMM.cs
    	Game/Assets/_Scripts/MichaelTest/cube_contMM.cs.meta
    	Game/Assets/_Scripts/MichaelTest/cube_contMR.cs
    	Game/Assets/_Scripts/MichaelTest/cube_contMR.cs.meta
    	Game/Assets/_Scripts/MichaelTest/cube_contUL.cs
    	Game/Assets/_Scripts/MichaelTest/cube_contUL.cs.meta
    	Game/Assets/_Scripts/MichaelTest/cube_contUM.cs
    	Game/Assets/_Scripts/MichaelTest/cube_contUM.cs.meta
    	Game/Assets/_Scripts/MichaelTest/cube_contUR.cs
    	Game/Assets/_Scripts/MichaelTest/cube_contUR.cs.meta
    	Game/Assets/_Scripts/MichaelTest/floor_puzzle_controller.cs
    	Game/Assets/_Scripts/MichaelTest/floor_puzzle_controller.cs.meta
    	Game/Game-csharp.sln
    	Game/Game.sln
    	Game/Game.userprefs
    	Game/ProjectSettings/QualitySettings.asset
    	Game/ProjectSettings/TagManager.asset
    
    Base for Demo2
    
    Made a base map for Demo2. Updated puzzle
    
    Adding Sihans files to my branch
    
    Bleh

---
+++ Game/Assets/_Scripts/lights_pulse/light_pulse4.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class light_pulse4 : MonoBehaviour {
+
+
+	public float smooth;
+	private float newIntensity;
+	public float endIntensity;
+	public float startIntensity;
+	
+	void Awake ()
+	{
+		newIntensity = light.intensity;
+	}
+	
+	void Update ()
+	{
+		if(light.intensity >= (endIntensity - 0.1f))
+		{
+			newIntensity = startIntensity;
+		}
+		else if(light.intensity <= (startIntensity + 0.1f))
+		{
+			newIntensity = endIntensity;
+		}
+		
+		IntensityChanging();
+	}
+	
+	void IntensityChanging ()
+	{
+		
+		light.intensity = Mathf.Lerp(light.intensity, newIntensity, smooth * Time.deltaTime);
+	}
+}
